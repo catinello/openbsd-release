179c9448d0274aa7041bd9bfea955788bde37f4f
Antonino Catinello <antonino@catinello.eu>
2023-02-21 15:07:07 +0100

    OpenBSD 7.2 errata 003_ukbd.patch

--- sys/dev/usb/ukbd.c
+++ sys/dev/usb/ukbd.c
@@ -131,7 +131,6 @@ struct ukbd_softc {
 
 	struct hidkbd		sc_kbd;
 	int			sc_spl;
-	struct hid_location	sc_apple_fn;
 
 #ifdef DDB
 	struct timeout		sc_ddb;	/* for entering DDB */
@@ -242,7 +241,7 @@ ukbd_attach(struct device *parent, struct device *self, void *aux)
 
 	if (uha->uaa->vendor == USB_VENDOR_APPLE) {
 		if (hid_locate(desc, dlen, HID_USAGE2(HUP_APPLE, HUG_FN_KEY),
-		    uha->reportid, hid_input, &sc->sc_apple_fn, &qflags)) {
+		    uha->reportid, hid_input, &kbd->sc_fn, &qflags)) {
 			if (qflags & HIO_VARIABLE) {
 				switch (uha->uaa->product) {
 				case USB_PRODUCT_APPLE_FOUNTAIN_ISO:
